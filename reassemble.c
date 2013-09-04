48cdf4d08b7cfd87bc4fb5a0ee99b358f51b8538
Kia <2592730+superbaud@users.noreply.github.com>
2013-09-04 15:59:50 -0700

    added a comment

--- reassemble.c
+++ reassemble.c
@@ -6,3 +6,31 @@
  */
 
 #include "mimocat.h"
+
+
+/* the way this code works is that it keeps two structures around -- one with 
+   information on what's already been reassembled and one with information on
+   things which haven't been reassembled yet
+   
+   the former structure is called reassembly_state, and the latter structure is
+   just simply the unpacked_cell structure that unpack_cell() writes to.
+
+   unpack_cell() doesn't handle anything to do with either the payload nor the
+   unpacked_cell->next pointer, it's up to us to do so here.
+ 
+ 
+ */
+
+
+
+
+
+
+
+typedef struct reassembly_state {
+    
+} reassembly_state_t;
+
+
+
+
