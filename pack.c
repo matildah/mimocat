df5be38c3436397f0071c5fbf708c77c2cb9cea5
Kia <2592730+superbaud@users.noreply.github.com>
2013-12-18 23:11:19 -0800

    added a main() that checks the code in pack.c

--- pack.c
+++ pack.c
@@ -53,11 +53,30 @@ void unpack_header(uint8_t *data, size_t len, unpacked_chunk_t *dest)
 }
 
 
-int main() {
+int main() {  
+    chunk_hdr_t *from = malloc(sizeof(chunk_hdr_t));
+    packed_chunk_t *to= malloc(sizeof(packed_chunk_t));
+    unpacked_chunk_t *check= malloc(sizeof(unpacked_chunk_t));
+
+    from->index     = 69;
+    from->begin_off = 0xdeadbeef;
+    from->end_off   = 0xdeadcafe;
+    from->seq       = 0xdeaddead;
+
+    to->data = malloc(CHUNK_HDR_LEN);
+
+    pack_header(from, to);
+
+    unpack_header(to->data, to->len, check);
+    assert(check->info.seq == from->seq);
+    
+    free(from);
+    free(to);
+    free(check);
     return 0;
 }
 
-    
+
 
 
 
