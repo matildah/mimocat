0da2f0ea27f738dcb7c98c9c6ada483014b8f198
Kia <2592730+superbaud@users.noreply.github.com>
2013-12-20 22:10:55 -0800

    added a utility function that just runs send(2) in a loop until all is sent

---
+++ net.c
@@ -0,0 +1,27 @@
+/* this file contains functions that make dealing with the network a bit
+   easier */
+
+
+/* this function just runs send(2) in a loop until the entire buffer is sent or
+   an error happens */
+
+ssize_t send_all(int socket, const void *buffer, size_t length, int flag)
+{
+    size_t bytes_sent = 0;
+    size_t bytes_unsent = length;
+
+    int sent;
+
+    while (bytes_sent < length)
+    {
+        sent = send(buffer + bytes_sent, bytes_unsent, flag);
+        if (sent == -1) 
+            return -1
+        bytes_sent += sent;
+        bytes_unsent -= sent;
+    }
+    
+    return bytes_sent;
+}
+
+
