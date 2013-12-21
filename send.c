10020bac84891fa72ae1e3d56424e579538bc86d
Kia <2592730+superbaud@users.noreply.github.com>
2013-12-20 22:57:15 -0800

    makes send_chunk behave correctly (namely, it sends the right stuff into the right file descriptors

--- send.c
+++ send.c
@@ -2,50 +2,55 @@
 
 #include "state.h"
 
-/* this function sends a chunk of data down the next appropriate fd */
+/* this function sends a chunk of data down the next appropriate fd and a
+   corresponding chunk header down the control connection */
 int send_chunk(FD_ARRAY *fdstate, uint8_t *data, size_t len)
 {
     CHUNK_HDR ourheader;
     PACKED_CHUNK packedchunk;
     int fd;
-    uint8_t *buf;
-    size_t buflen;
-    assert(fdstate -> numfds != 0);
+    assert(fdstate -> numfds > 0); /* make sure we have FDs ready */
 
 
-    buflen = len + CHUNK_HDR_LEN;
-    assert(buflen > len && buflen > CHUNK_HDR_LEN);
-    buf = malloc(buflen);
-    assert (buf != NULL);
-
-    fd = fdstate->fds[fdstate->nextidx];
-
+    fd = fdstate->fds[fdstate->nextidx]; /* which FD we use */
+    
+    /* we prepare the chunk header */
     ourheader.index = fdstate->fds[fdstate->nextidx];
     ourheader.begin_off = fdstate->bytes[fdstate->nextidx];
     ourheader.end_off = ourheader.begin_off + len;
-
     ourheader.seq = fdstate->nextseq;
 
+    /* and allocate space for the packed version of it */
     packedchunk.data = malloc(CHUNK_HDR_LEN);
     assert(packedchunk.data != NULL);
     
+    /* and we pack it */
     pack_header(&ourheader, &packedchunk);
-
-    memcpy(buf,                   packedchunk.data, packedchunk.len);
-    memcpy(buf + packedchunk.len, data,             len);
+    /* and we send it down the control connection */
+    if(send_all(fdstate->controlfd, packedchunk.data, packedchunk.len, 0) == -1)
+    {
+        perror("send error in control connection");
+        exit(-1);
+    }
+    /* it's sent, so we don't need its packed version */
     free(packedchunk.data);
 
-    if(send_all(fd, buf, buflen, 0) == -1)
+    /* ...and we send the corresponding data */
+    if(send_all(fd, data, len, 0) == -1)
     { 
-        perror("send error");
+        perror("send error in data connection");
         exit(-1);
     }
 
+    /* here we update fdstate */
+    fdstate->bytes[fdstate->nextidx] += len; /* how many bytes we sent down that fd*/
+    /* we update nextidx while making sure it doesn't get too big */
     fdstate->nextidx++;
-    if(fdstate->nextidx > fdstate->numfds - 1)
+    if (fdstate->nextidx > (fdstate->numfds - 1))
     {
         fdstate->nextidx = 0;
     }
+
 
     fdstate->nextseq++;
 }
